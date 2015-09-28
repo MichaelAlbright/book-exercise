6cb4570a684c930dbb29994291b3f86cf9ff25d6
MichaelAlbright <michaelalbright25@yahoo.com>
2015-09-28 13:31:11 -0400

    2.84

--- Book.java
+++ Book.java
@@ -31,4 +31,14 @@ class Book
     {
        return title;
     }
+    
+    public void printAuthor()
+    {
+        System.out.println(author);
+    }
+    
+    public void printTitle()
+    {
+        System.out.println(title);
+    }
 }
